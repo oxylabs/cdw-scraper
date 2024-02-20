8c05802e8bf57bf4f0aaca45c59922c8288257ef
AugustOxy <129506779+AugustOxy@users.noreply.github.com>
2024-02-20 14:31:30 +0200

    Adding code examples/cdw_curl.sh

---
+++ code examples/cdw_curl.sh
@@ -0,0 +1,4 @@
+curl --user user:pass \
+'https://realtime.oxylabs.io/v1/queries' \
+-H "Content-Type: application/json" \
+-d '{"source": "universal_ecommerce", "url": "https://www.cdw.com/search/cables/?key=&w=b"}'
